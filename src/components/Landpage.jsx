7dae86652394428e3e8b19f5b52176033aa01b10
KHOBBY POUNDS <khobbypounds@JOSEPHs-MacBook-Pro.local>
2023-01-18 06:19:11 +0000

    update 3.6

--- src/components/Landpage.jsx
+++ src/components/Landpage.jsx
@@ -16,10 +16,14 @@ const Landpage = () => {
             </motion.div>
         
 
-            <p className='text-md md:text-2xl md:w-2/3 flex justify-center md:justify-start'> Here is your friendly and experienced website and development experts! 
+            <motion.p 
+                animate={{x: 0}} 
+                initial={{x:-5000}}
+                transition={{delay:0.5, duration:1}}
+            className='text-md md:text-2xl md:w-2/3 flex justify-center md:justify-start'> Here is your friendly and experienced website and development experts! 
                 Whether you're a small business, a large corporation, or any organization in between, we can help you create 
                 the perfect website to fit your needs. With our extensive knowledge of coding, design, and development, we have the 
-                perfect team to make your web dreams come true. Get in touch today to learn more about how we can help you.</p>
+                perfect team to make your web dreams come true. Get in touch today to learn more about how we can help you.</motion.p>
             
             <div className='flex pt-4 justify-evenly'>
                 <div className='gap-5 flex'>
